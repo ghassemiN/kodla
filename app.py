254e3aafb69d77e3f62f1a71852d4fef2c59fd62
ghassemin <nasy.ghassemi@gmail.com>
2022-02-06 14:02:29 +0300

    created new functions for validation and spelling aplabeth :  validator and spelling_alphabet defined new function to take and response json : api_kodla

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, escape
+from flask import Flask, render_template, request, escape, jsonify
 from letters import *
 from gtts import gTTS
 from playsound import playsound
-import random, string
+import random, string, json
 
 
 
@@ -22,35 +22,66 @@ def play_word(coded_string):
     myobj.save("static/{}.mp3".format(file_name))
     return file_name
 
-
-@app.route("/", methods=['POST'])
-# Decode turkish words
-def coding():
-    my_word = request.form['words']
-    coded_string = ""
-
-    for i in my_word:
+# Validates user entry
+def validator(a_word):
+    print (a_word)
+    for i in a_word:
         if i in lower_upper:
             i = lower_upper[i]
         
         if i not in replace_letters:
-          return render_template('index.html',err_msg= " Your entry could involve : lower or upper letters/numbers/_/-/space")
+          return "Your entry could involve : lower or upper letters/numbers/_/-/space"
 
-    if len(my_word)!= 0 or len(my_word) > 30:
-        for i in range(len(my_word)):
-            if i == 0:
-                coded_string = coded_string + replace_letters[my_word.upper()[i]]
-            else:
-                coded_string = coded_string + " , " + replace_letters[my_word.upper()[i]]
+    if len(a_word) == 0 or len(a_word) > 30:
+        return "Your entry should be between 1-30 letters"
+    else:
+        return 200
+        
+# Spells the word given to it 
+def spelling_alphabet(a_word):
 
-        file_name = play_word(coded_string)
-        return render_template('index.html',coded_string= escape(coded_string) , file_name=file_name,my_word=my_word)
-    else: 
-        return render_template('index.html',err_msg="Check your entry, it should be between 1-30 letters")
+    coded_string = ""
+    for i in range(len(a_word)):
+        if i == 0:
+            coded_string = coded_string + replace_letters[a_word.upper()[i]]
+        else:
+            coded_string = coded_string + " , " + replace_letters[a_word.upper()[i]]
+    return coded_string
+    
 
 
+@app.route("/", methods=['POST'])
+# Get the word/words and response 
+def kodla():
+    my_word = request.form['words']
+    my_result = validator(my_word)
+    if my_result != 200:
+        return render_template('index.html',err_msg= my_result )
+    
+    spelled = spelling_alphabet(my_word)
+    file_name = play_word(spelled)
+    return render_template('index.html',coded_string= escape(spelled) , file_name=file_name, my_word=my_word)
 
 
+# api spelling alphabet
+@app.route("/api", methods=['POST']) 
+def api_kodla():
+    data = json.dumps(request.get_json())
+    items = json.loads(data)
+
+    my_word = items["my_word"]
+
+    my_result = validator(my_word)
+    if my_result != 200:
+        return jsonify({'message' : my_result }),400
+    
+    spelled = spelling_alphabet(my_word)
+    file_name = play_word(spelled)
+    result = {
+        'coded_string' : spelled,
+        'audio_file_path'    : "static/"+file_name+".mp3",
+    }
+    return jsonify({'message' : result }),200
 
 if __name__ == "__main__":
     app.run()
