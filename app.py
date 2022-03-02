97294eb0217d44ff5c3823b92a1ca50263233906
ghassemin <nasy.ghassemi@gmail.com>
2022-03-02 11:12:03 +0300

    I just removed setebhook endpoint, I set it manually

--- app.py
+++ app.py
@@ -109,18 +109,6 @@ def response():
 
     return 'ok'
 
-# set webhook
-@app.route('/setwebhook', methods=['GET', 'POST'])
-def set_webhook():
-    # we use the bot object to link the bot to our app which live
-    # in the link provided by URL
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    # something to let us know things work
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
-
 
 if __name__ == "__main__":
     app.run()
